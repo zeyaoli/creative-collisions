c203ded6542cd4476d2dcbd61a9148621b7f311e
Sylvan Zheng <squidgetx@gmail.com>
2020-02-16 15:36:18 -0500

    flexible number of participants + bugfixes

--- public/output/sketch.js
+++ public/output/sketch.js
@@ -1,61 +1,69 @@
 // Open and connect output socket
 let socket = io('/output');
+let displayTextString = '';
+let users = {};
 
 // Listen for confirmation of connection
 socket.on('connect', function() {
 	console.log('Connected');
+ 
+  socket.on('userList', (message) => {
+    users = message
+
+    if (gameState == 'WAITING') {
+      displayTextString = `${Object.keys(users).length} connected`
+    }
+  })
+
 });
 
-let users = {};
-let participants = 10;
 let canClick = false;
 let ranNum;
-let count, result;
-let displayText;
+let result = '';
+let count = 0;
 
 let sec = 5;
-let isInGame = false;
+// WAITING, INGAME, FINISHED
+let gameState = 'WAITING';
 let loseArr = [];
 
 function setup() {
-	noCanvas();
-	let startButton = createButton('start game');
-	startButton.mousePressed(startGame);
 	displayText = createP('');
 	displayText.class('content');
+  let startButton = createButton('start game');
+	startButton.mousePressed(startGame);
 
 	socket.on('inputValue', (message) => {
 		let id = message.id;
 		let data = message.data;
 
-		if (!(id in users)) {
-			users[id] = {
-				value : data
-			};
-		}
+    users[id] = {
+      value : data
+    };
+
 		count = 0;
 		for (id in users) {
 			if (users[id].value == 1) {
 				count += 1;
 			}
 		}
+
 		count == ranNum ? (result = `Wow`) : (result = `Uh Oh`);
-		loseArr = [
-			`${result} y'all failed, ${count} people clicked`,
-			`Only ${count} people clicked this time, try it again!`,
-			`Hmm, not good, ${count} people clicked`
-		];
 	});
 }
 
+function draw() {
+  displayText.html(displayTextString);
+}
+
 function startGame() {
-  if (isInGame === true) {
+  if (gameState === 'INGAME') {
     // do nothing
     console.log('game already started')
     return;
   }
 
-	isInGame = true;
+	gameState = 'INGAME';
 	randomNum();
 	let timer = setInterval(() => {
 		sec--;
@@ -64,8 +72,8 @@ function startGame() {
 			clearInterval(timer);
 			canClick = false;
 			sec = 5;
-			replaceText();
-			isInGame = false;
+      gameState = 'FINISHED'
+      replaceText()
 		}
 		socket.emit('click', canClick);
 		console.log(sec);
@@ -73,16 +81,29 @@ function startGame() {
 }
 
 function randomNum() {
-	ranNum = floor(random(2, participants));
-	displayText.html(`In this game, ${ranNum} people have to click their phone`);
+	ranNum = floor(random(2, Object.keys(users).length));
+	displayTextString = `In this game, ${ranNum} people have to click their phone`;
 }
 
 function replaceText() {
-	if (count == ranNum) {
-		displayText.html(`${result}, you guys finally made it!!`);
+	if (count === ranNum) {
+		displayTextString = `${result}, you guys finally made it!!`;
 	} else {
-		displayText.html(loseArr[Math.floor(random(2))]);
+    loseArr = [
+			`${result} y'all failed, ${count} people clicked`,
+			`Only ${count} people clicked this time, try it again!`,
+			`Hmm, not good, ${count} people clicked`
+		];
+		displayTextString = loseArr[Math.floor(random(2))];
 	}
+}
+
+function touchStarted() {
+  console.log('touchstarted')
+  if (gameState == 'FINISHED') {
+    gameState = 'WAITING'
+    displayTextString = `${Object.keys(users).length} connected`
+  }
 }
 
 // set a boolean for not clicking start button while the program is running
